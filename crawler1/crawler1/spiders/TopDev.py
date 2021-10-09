ac6c54041f2241f35405d5c26e02d8d8b31fb87a
locnv <nguyenvanloc432k@gmail.com>
2021-10-09 23:53:25 +0700

    crawl TopDev

--- crawler1/crawler1/spiders/TopDev.py
+++ crawler1/crawler1/spiders/TopDev.py
@@ -5,25 +5,42 @@ from crawler1.items import Crawler1Item
 class TopdevSpider(scrapy.Spider):
     name = 'TopDev'
     # allowed_domains = ['https://topdev.vn/blog/motion-graphics-design-la-gi/']
-    start_urls = ['https://topdev.vn/blog/category/lap-trinh/mobile/']
+    start_urls = ['https://topdev.vn/blog/category/lap-trinh/ai-machine-learning/#',
+    'https://topdev.vn/blog/category/lap-trinh/mobile/#',
+    'https://topdev.vn/blog/category/lap-trinh/frontend/#',
+    'https://topdev.vn/blog/category/lap-trinh/backend/#',
+    'https://topdev.vn/blog/category/lap-trinh/fullstack/#',
+    'https://topdev.vn/blog/category/lap-trinh/product/#',
+    'https://topdev.vn/blog/category/lap-trinh/devops/#',
+    'https://topdev.vn/blog/category/cong-nghe/#',
+    'https://topdev.vn/blog/category/chuyen-gia-noi/#',
+    'https://topdev.vn/blog/category/phong-van/#']
 
     def parse(self, response):
-        listLink = response.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div > div.td-pb-span8.td-main-content > div > div:nth-child(2) > div.td-module-thumb > a::attr(href)').extract()
+        listLink = response.css('div.td_module_10.td_module_wrap.td-animation-stack > div.item-details > h3 > a::attr(href)').extract()
 
-        print(listLink)
+        # print(listLink)
 
-    #     for link in listLink:
-    #         yield scrapy.Request(link, callback=self.crawlData)
+        for link in listLink:
+            yield scrapy.Request(link, callback=self.crawlData)
 
-    # def crawlData(self, response):
-    #     # print(response.url)
-    #     # print(response.css('#ftwp-postcontent > p:nth-child(1)').extract())
-    #     # print(response.css('title::text').extract())
+    def crawlData(self, response):
+        # print(response.url)
+        # print(response.css('#ftwp-postcontent > p:nth-child(1)').extract())
+        # print(response.css('title::text').extract())
 
-    #     item = Crawler1Item()
+        item = Crawler1Item()
 
-    #     item['link'] = response.url 
-    #     item['title'] = response.css('title::text').extract()
-    #     item['about'] = response.css('#ftwp-postcontent > p:nth-child(1) > span:nth-child(1)::text').extract()
+        item['link'] = str(response.url) 
+        item['title'] = str(response.css('head > title::text').extract_first())
+        item['about'] = str(response.css('#ftwp-postcontent > p > span::text').extract_first())
 
-    #     print(item)
+        print(item)
+        yield item 
+
+        listSubLink = response.css('div.td-related-span4 > div > div.td-module-image > div.td-module-thumb > a::attr(href)').extract()
+
+        # print(listLink)
+
+        for link in listSubLink:
+            yield scrapy.Request(link, callback=self.crawlData)
